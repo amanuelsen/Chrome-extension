c87357c258d6814e6f775f82109f30430494fe54
senai <saniy.cr7@gmail.com>
2023-06-15 10:43:59 +0100

    last uppdate

--- indes.js
+++ indes.js
@@ -2,34 +2,63 @@ let button = document.getElementById("btn");
 let input = document.getElementById("input");
 let ul = document.getElementById("ul");
 let myleads = [];
+let allleads=[];
+let click=0;
+let deletee = document.getElementById("delete");
+let tab= document.getElementById("tab")
+
+
+
+tab.addEventListener("click", ()=>{
+  myleads.push(window.location.href);
+
+  localStorage.setItem("myleads", JSON.stringify(myleads));
+
+  input.value = "";
+  render(myleads);
+}) 
+
+deletee.addEventListener("dblclick", function(){
+  
+ 
+  
+    localStorage.clear()
+    myleads=[]
+    render(myleads)
+
+  
+})
 // myleads=JSON.stringify(myleads)
 
 // myleads= JSON.parse(myleads)
 // myleads.push("senai")
 // console.log(myleads)
 let listitem= ""
-let leadfromlocalstorage= JSON.parse(localStorage.getItem("myleads"))
+const leadfromlocalstorage= JSON.parse(localStorage.getItem("myleads"))
 
 
 if(leadfromlocalstorage){
   myleads=leadfromlocalstorage
-  renderlead()
+  render(myleads)
 }
+
+function render(leads){
+  for (let i = 0; i < leads.length; i++) {
+      listitem += `<li> <a target="_blank "href=${leads[i]}>` + leads[i] +"</a></li>";
+    
+    }
+    ul.innerHTML= listitem
+    listitem= ""
+    
+}
+
 button.addEventListener("click", function() {
   myleads.push(input.value);
-  
-  input.value = ""; 
 
-  localStorage.setItem("myleads", JSON.stringify(myleads))
-  renderlead()
+  localStorage.setItem("myleads", JSON.stringify(myleads));
+
+  input.value = "";
+  render(myleads);
 });
-function renderlead(){
-    for (let i = 0; i < myleads.length; i++) {
-        listitem += `<li> <a target="_blank "href=${myleads[i]}>` + myleads[i] +"</a></li>";
-      
-      }
-      ul.innerHTML= listitem
-      listitem= ""
-      
-}
- 
+
+  
